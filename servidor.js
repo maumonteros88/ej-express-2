944919e74fce69b7c2845e8943075ccb34b391b0
Mauricio <mau.monteros88@gmail.com>
2021-05-08 18:15:25 -0300

    Se ordena codigo y constantes

--- servidor.js
+++ servidor.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+
 const port = 3000;
+const dirIndex = path.join(__dirname, "index.html");
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(dirIndex);
 });
 
 app.listen(port, () => {
